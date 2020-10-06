b244296e6ba47f2b02056be1a1653833fca26867
Puneeth P <puneeth1996p@gmail.com>
2020-10-06 10:43:08 +0530

    quick commit

--- my-news/src/App.js
+++ my-news/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Card, Dropdown } from 'react-bootstrap';
 import './App.css';
 
 
@@ -27,7 +28,30 @@ export default class App extends Component {
         {this.state.articles.map( (itm, index) => (
           
             <>
-              <h2>{itm.title}</h2>
+              <Card className="bg-dark text-white">
+                <Card.Img src={itm.urlToImage} style={ {  width: 200, height: 120, } } alt="Card image" />
+                <Card.ImgOverlay>
+                  <Card.Title>{itm.title}</Card.Title>
+                  <Card.Text>
+                    {itm.description}
+                  </Card.Text>
+                  <Card.Text>{itm.publishedAt}</Card.Text>
+                </Card.ImgOverlay>
+              </Card>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={itm.urlToImage} />
+                <Card.Body>
+                  <Card.Title>{itm.title}</Card.Title>
+                  <Card.Text>{itm.description}</Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>{itm.publishedAt}</ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                  <Card.Link href="#">Card Link</Card.Link>
+                  <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+              </Card>
             </>
           
         ) )}
