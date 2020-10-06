ea3bf3eb2dcdec94f8fa95704e26661962744bb5
Puneeth P <puneeth1996p@gmail.com>
2020-10-06 10:32:00 +0530

    basic request is surely made

--- my-news/src/App.js
+++ my-news/src/App.js
@@ -3,10 +3,34 @@ import './App.css';
 
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      articles: [],
+    }
+
+  }
+
+  componentDidMount(){
+    fetch('http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=24317ff2add448729f246e75e63393e7')
+    .then( (resp) => {
+      return resp.json();
+    })
+    .then( (myJson) => {
+      this.setState({ articles: myJson.articles });
+    });
+  }
+  
   render() {
     return (
       <div>
-        Hellow Class Based Component
+        {this.state.articles.map( (itm, index) => (
+          
+            <>
+              <h2>{itm.title}</h2>
+            </>
+          
+        ) )}
       </div>
     )
   }
