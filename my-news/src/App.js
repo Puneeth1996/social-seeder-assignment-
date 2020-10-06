812ff062b889c27a5fddf00021d86404986279a5
Puneeth P <puneeth1996p@gmail.com>
2020-10-06 10:03:54 +0530

    quick commit with code

--- my-news/src/App.js
+++ my-news/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      Hellow
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hellow Class Based Component
+      </div>
+    )
+  }
+}
