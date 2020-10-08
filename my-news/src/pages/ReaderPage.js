e4194157c49b615e453ddcb0691d164aa3f642b2
Puneeth P <puneeth1996p@gmail.com>
2020-10-08 11:42:41 +0530

    working on the readers page

--- my-news/src/pages/ReaderPage.js
+++ my-news/src/pages/ReaderPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button,   } from 'react-bootstrap';
+import { Button, Image,    } from 'react-bootstrap';
 
 export default class ReaderPage extends Component {  
     render() {
@@ -13,14 +13,33 @@ export default class ReaderPage extends Component {
             publishedAt,
             content,
         } = this.props.readerArr  
+
+        
+        const mybutton =  {
+            position: 'fixed',
+            bottom: '-4px',
+            right: '10px',
+        }
+
+        const feedback =  {
+            backgroundColor : '#4acd7f',
+            color: 'white',
+            padding: '10px 20px',
+            borderRadius: '8px',
+            borderColor: '#46b8da',
+        }
         return (
             <div>
-                <div>
-                    <Button variant="outline-secondary"  style={{float: 'right', margin: '10px'}}   onClick={ this.props.handleBackButton  }  >Back To Home</Button>{' '}
+                <div style={mybutton}>
+                    <button style={feedback}   onClick={ this.props.handleBackButton } >Bock To Home</button>
                 </div>
-                <div  style={{ marginTop: '10rem' }}   >
-                    <h1>{title}</h1>
+                <div  style={{ margin: '2rem 2rem 2rem 2rem' }}   >
+                    <h1 style={{ color: '#000000bd' , margin: '0 0 2rem 0'  }}  >{title}</h1>
+                    <Image src={urlToImage} style={{ height: '60vh', width: '100vw' }} fluid />
+                    <h3  style={{ margin: '1rem 2rem 6rem 2rem' }}  >{description}</h3>
+                    <p  style={{ margin: '1rem 4rem 6rem 4rem' }}  >{content}{content}{content}{content}{content}{content}{content}{content}{content}{content}</p>
                 </div>
+
             </div>
         )
     }
