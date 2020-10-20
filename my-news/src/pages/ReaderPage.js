51011ecb679f2284b8fe08ee91a6ad111d1f83d4
PUNEETH P <38997799+Puneeth1996@users.noreply.github.com>
2020-10-20 10:18:19 +0530

    Removed the warnings shoes

--- my-news/src/pages/ReaderPage.js
+++ my-news/src/pages/ReaderPage.js
@@ -4,7 +4,6 @@ import {  Image,    } from 'react-bootstrap';
 export default class ReaderPage extends Component {  
     render() {
         const {
-            source, 
             author, 
             title, 
             description, 
